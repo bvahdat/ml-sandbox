8012b73d57182361dd38b3cb131a5a8caf0dd50c
Babak Vahdat <bvahdat@apache.org>
2023-03-02 11:51:19 +0100

    features engineering for 'Title'

--- kaggle/titanic/titanic.py
+++ kaggle/titanic/titanic.py
@@ -16,18 +16,40 @@ from sklearn.svm import SVC, NuSVC, LinearSVC
 from xgboost import XGBClassifier
 
 train_data = pd.read_csv('http://bit.ly/kaggletrain')
+test_data = pd.read_csv('http://bit.ly/kaggletest')
 
-X = train_data.drop(columns=['PassengerId', 'Name', 'Ticket', 'Cabin', 'Survived'])
 y = train_data.Survived
+
+
+def prepare_features(train_df, test_df):
+    prepared = []
+    for dataframe in [train_df, test_df]:
+        copy = dataframe.copy()
+        copy['Title'] = copy.Name.str.extract('([A-Za-z]+)\.', expand=False)
+        copy['Title'] = copy['Title'].replace(['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Infrequent')
+        copy['Title'] = copy['Title'].replace('Mlle', 'Miss')
+        copy['Title'] = copy['Title'].replace('Ms', 'Miss')
+        copy['Title'] = copy['Title'].replace('Mme', 'Mrs')
+        copy.drop(columns=['PassengerId', 'Name', 'Ticket', 'Cabin'], inplace=True)
+        prepared.append(copy)
+    return prepared
+
+
+X, X_test = prepare_features(train_data, test_data)
+
+# drop the target label from the training set
+X.drop(columns=['Survived'], inplace=True)
+
+# drop that one single test sample at index 152 with Fare = NaN
+X_test.ffill(inplace=True)
 
 
 def create_pipeline(model):
     column_transformers = make_column_transformer(
         (make_pipeline(SimpleImputer(), StandardScaler()), ['Age']),
         (make_pipeline(SimpleImputer(strategy='most_frequent'), OneHotEncoder()), ['Embarked']),
-        (make_pipeline(OneHotEncoder()), ['Sex']),
-        (make_pipeline(StandardScaler()), ['Fare']), # don't scale 'SibSp', 'Parch', 'Pclass' as it would reduce the accuracy on the test set!
-        remainder='passthrough')
+        (make_pipeline(OneHotEncoder()), ['Sex', 'Title']),
+        (make_pipeline(StandardScaler()), ['Fare', 'Parch', 'Pclass', 'SibSp']))
     return make_pipeline(column_transformers, model)
 
 
@@ -39,7 +61,7 @@ def train(X, y, model, scores_dict=None):
         scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
         score_mean = scores.mean()
         score_std = scores.std()
-        scores_dict[model_name] = score_mean
+        scores_dict[score_mean] = model_name
         print(f'accuracy of the model {model_name}: {score_mean:.3f} (+/- {score_std:.3f})')
     else:
         pipeline.fit(X, y)
@@ -50,21 +72,21 @@ def train(X, y, model, scores_dict=None):
 
 scores_mean = {}
 for model in [
-    SVC(),
-    NuSVC(),
+    BaggingClassifier(KNeighborsClassifier(n_neighbors=3)),
+    ExtraTreesClassifier(n_estimators=2000),
+    GaussianNB(),
     LinearSVC(max_iter=4000),
     LogisticRegression(),
-    GaussianNB(),
-    BaggingClassifier(KNeighborsClassifier(n_neighbors=3)),
+    NuSVC(),
     RandomForestClassifier(n_estimators=2000),
-    ExtraTreesClassifier(n_estimators=2000),
     SGDClassifier(max_iter=4000),
+    SVC(),
     XGBClassifier(n_estimators=2000)
 ]:
     train(X, y, model, scores_mean)
 
-best_model = max(scores_mean)
-print(f'best model score is through {best_model} with a mean of {scores_mean[best_model]:.3f}')
+best_score = max(scores_mean)
+print(f'best model score is through {scores_mean[best_score]} with a mean of {best_score:.3f}')
 
 # SVC seems to achieve the best accuracy
 param_grid = {'C': [0.1, 1, 10, 100, 1000],
@@ -79,10 +101,6 @@ y_pred = pipeline.predict(X)
 print('confusion matrix:', LS, confusion_matrix(y, y_pred))
 print('classification report:', LS, classification_report(y, y_pred))
 
-test_data = pd.read_csv('http://bit.ly/kaggletest')
-
-X_test = test_data.drop(columns=['PassengerId', 'Name', 'Ticket', 'Cabin'])
-X_test.ffill(inplace=True)
 y_test = pipeline.predict(X_test)
 
 passengerId = test_data.PassengerId
